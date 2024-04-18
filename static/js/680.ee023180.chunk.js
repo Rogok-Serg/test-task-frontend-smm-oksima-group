0e90e8c3e50b62e5099cb61c6a4153a7f916a56a
Rogok-Serg <116109922+Rogok-Serg@users.noreply.github.com>
2024-04-18 15:12:02 +0000

    Deploying to gh-pages from @ Rogok-Serg/test-task-frontend-smm-oksima-group@7afdf995ca2ea77a5cf5f9ef5f2fc676477705c7 🚀

--- static/js/680.ee023180.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_task_frontend_smm_oksima_group=self.webpackChunktest_task_frontend_smm_oksima_group||[]).push([[680],{680:function(e,s,n){n.r(s);var t=n(184);s.default=function(){return(0,t.jsxs)("div",{children:[(0,t.jsx)("h2",{children:"Home Page"}),(0,t.jsx)("p",{children:"I am Home"})]})}}}]);
-//# sourceMappingURL=680.ee023180.chunk.js.map
